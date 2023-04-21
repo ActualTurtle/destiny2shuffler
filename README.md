570608530cdd0cddc4a21925c9dce0539dd108b2
Drew Tuttle <dt0700@gmail.com>
2023-04-21 15:01:36 -0600

    Project initialized

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 # Destiny 2 Loadout Shuffler
 
 
+Important links:
+
+[Project Management - Miro](https://miro.com/app/board/uXjVOqit89s=/)
+[Destiny 2 API docs](https://bungie-net.github.io/multi/index.html#about-connecting)
 
---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Nxdzv9KzDzI5mSGaxYctQ-hRg2p-Y8E",
+  authDomain: "destiny2-shuffler.firebaseapp.com",
+  projectId: "destiny2-shuffler",
+  storageBucket: "destiny2-shuffler.appspot.com",
+  messagingSenderId: "345218062326",
+  appId: "1:345218062326:web:af4e98305104cf73dfd9d7",
+  measurementId: "G-ZE7HMGNGFV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
