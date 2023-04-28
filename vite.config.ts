1a9e27f7cb22c68f82bfb736aa0df4c73c91f372
Drew Tuttle <dt0700@gmail.com>
2023-04-28 15:08:33 -0600

    Better deploy code

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/actualturtle.github.io/destiny2shuffler/",
   plugins: [react()],
   build: {
     outDir: "docs"
   },
-  base: "./"
 })
