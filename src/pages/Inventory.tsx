0de5c5a263ed63d9155c94a9228a0ef2ce88c55b
Aryan Osqueezadeh <A02289878@aggies.usu.edu>
2023-05-01 10:51:52 -0600

    Updated Inventory Page

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -8,7 +8,19 @@ export const Inventory = () => {
             Stuff goes here, Inluding verification of authentication, eventually
         </div>
 
-        <Link to={`../loadouts`}> To Previous LoadOuts</Link>
+        <div className="flex center inventory-full">
+          <div>
+            <h3 className="randomize-button">Randomize Loadout</h3>
+          </div>
+          <div className="inventory">
+            <h3>Item 1</h3>
+            <h3>Item 2</h3>
+            <h3>Item 3</h3>
+            <h3>Item 4</h3>
+          </div>
+        </div>
+
+        <span><Link to={`../loadouts`}> To Previous LoadOuts</Link></span>
     </div>
   )
 }
