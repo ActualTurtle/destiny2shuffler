07042dea572b51309ffde7568a9a784e7e1ded20
Matthew Hill <potatoman.mh@gmail.com>
2023-05-02 15:33:37 -0600

    remove comments

--- src/dto/token.ts
+++ src/dto/token.ts
@@ -1,12 +1,9 @@
 export interface tokens {
   tokenType: string,
   accessToken: string | null,
-  //accessTokenReadyDate: 0,
   accessTokenExpiryDate: number,
   refreshToken: string | null,
-  //refreshTokenReadyDate: 0,
   refreshTokenExpiryDate: number,
   membershipId: number,
-  //scope: 0
 };
 
